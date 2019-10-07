e5e9160c162a59b811ed3e45b9e1e3b6c2baf89f
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-10-07 17:11:22 +0400

    commit 11.5

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
+
 const App = () => {
   useEffect(() => {
     /** Init Materialize JS */
     M.AutoInit()
   })
   return (
-    <div>
-      My App
-    </div>
+    <>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </>
   );
 }
 
