762e681ac5683c1454e965814e04e1d8f1be0baf
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-10-08 16:20:38 +0400

    commit 12.3

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -14,6 +14,7 @@ import {GET_LOGS, SET_LOADING, LOGS_ERROR} from './types'
 //   }
 // }
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading()
@@ -31,7 +32,7 @@ export const getLogs = () => async dispatch => {
       payload: err.response.data
     })
   }
-  
+
 }
 
 /** Set loading to true */
