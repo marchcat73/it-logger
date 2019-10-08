c170f3b5b5e2b7febca378e2fc32d67a0051e818
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-10-08 14:37:54 +0400

    commit 12.1

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from './components/techs/AddTechModal'
 import TechListModal from './components/techs/TechListModal'
+import {Provider} from 'react-redux'
+import store from './store'
 
 
 const App = () => {
@@ -18,17 +20,19 @@ const App = () => {
     M.AutoInit()
   })
   return (
-    <>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </>
+    <Provider store={store}>
+      <>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </>
+    </Provider>
   );
 }
 
